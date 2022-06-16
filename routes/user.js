e8c70055a7ce9cab1f355b4db502380fe926133b
Coulibaly Wawota aziz <azizcoulibaly009@gmail.com>
2022-06-16 03:21:21 +0000

    zeko-app

--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,7 @@ route.post('/singnin/',userControll.singnin,userControll.sendOtpCode)
 route.post('/login/',userControll.updateOtpcode,userControll.login)
 route.post('/getuser/',userAuth.authentification,userControll.getUser)
 route.put('/updateprofil/info/',userAuth.authentification,userControll.updateProfilInfo)
-route.post('/updateprofil/avatar/',userAuth.authentification,firebase,userControll.updateProfilAvatar)
+route.put('/updateprofil/avatar/',userAuth.authentification,firebase,userControll.updateProfilAvatar)
 route.put('/updateprofil/email/',userAuth.authentification,userControll.updateEmail,userControll.updateOtpcode)
 route.post('/otpcode/',userAuth.authentification,userControll.updateOtpcode,userControll.sendOtpCode)
 
