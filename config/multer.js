4a75c95f7a383c226254f92ba38e2236920863a4
Coulibaly Wawota aziz <azizcoulibaly009@gmail.com>
2022-06-19 01:04:31 +0000

    zeko-app

--- config/multer.js
+++ config/multer.js
@@ -1,8 +1,5 @@
-
-const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-key.json");
 const multer = require('multer');
-const BUCKET = 'zeko-app-1b172.appspot.com'
+
 const MINE_TYPES = {
     'image/jpg':'jpg',
     'image/jpeg':'jpeg',
@@ -10,11 +7,6 @@ const MINE_TYPES = {
     'image/gif':'gif'
     
 }
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket : BUCKET
-});
 
 const storage = multer.diskStorage({
     destination : (requ,file,callback)=>{
